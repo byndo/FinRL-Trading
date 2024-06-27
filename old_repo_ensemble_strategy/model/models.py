ffdf0771de56d9f3920df715db74a8c0286809e4
rabaiand <andras.rabai@gmail.com>
2024-06-27 09:24:23 +0100

    first round of changes; some trained models as well but they are probably not needed

--- old_repo_ensemble_strategy/model/models.py
+++ old_repo_ensemble_strategy/model/models.py
@@ -1,3 +1,9 @@
+# THIS IS THE NEW CODE THAT I NEED TO WORK ON
+import sys
+path = '/Users/andrasrabai/Dissertation/new_code/FinRL-Trading/old_repo_ensemble_strategy'
+if path not in sys.path:
+    sys.path.append('/Users/andrasrabai/Dissertation/new_code/FinRL-Trading/old_repo_ensemble_strategy')
+
 # common library
 import pandas as pd
 import numpy as np
@@ -5,15 +11,15 @@ import time
 import gym
 
 # RL models from stable-baselines
-from stable_baselines import GAIL, SAC
-from stable_baselines import ACER
+# from stable_baselines import GAIL, SAC
+# from stable_baselines import ACER
 from stable_baselines import PPO2
 from stable_baselines import A2C
 from stable_baselines import DDPG
-from stable_baselines import TD3
+# from stable_baselines import TD3
 
 from stable_baselines.ddpg.policies import DDPGPolicy
-from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy
+from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy, FeedForwardPolicy, register_policy
 from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
 from stable_baselines.common.vec_env import DummyVecEnv
 from preprocessing.preprocessors import *
@@ -24,10 +30,18 @@ from env.EnvMultipleStock_train import StockEnvTrain
 from env.EnvMultipleStock_validation import StockEnvValidation
 from env.EnvMultipleStock_trade import StockEnvTrade
 
+# AR: this is new code
+class custom_policy(FeedForwardPolicy):
+    def __init__(self, *args, **kwargs):
+        super(custom_policy, self).__init__(
+            *args,
+            **kwargs,
+            net_arch=[dict(pi=[128, 128, 128], vf=[128, 128, 128])],
+            feature_extraction='mlp'
+        )
 
 def train_A2C(env_train, model_name, timesteps=25000):
     """A2C model"""
-
     start = time.time()
     model = A2C('MlpPolicy', env_train, verbose=0)
     model.learn(total_timesteps=timesteps)
@@ -37,16 +51,17 @@ def train_A2C(env_train, model_name, timesteps=25000):
     print('Training time (A2C): ', (end - start) / 60, ' minutes')
     return model
 
-def train_ACER(env_train, model_name, timesteps=25000):
+# AR added
+def train_A2C_custom_policy(env_train, model_name, timesteps=25000):
+    """A2C model"""
     start = time.time()
-    model = ACER('MlpPolicy', env_train, verbose=0)
+    model = A2C(custom_policy, env_train, verbose=0)
     model.learn(total_timesteps=timesteps)
     end = time.time()
 
     model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
-    print('Training time (A2C): ', (end - start) / 60, ' minutes')
+    print('Training time (A2Ccp): ', (end - start) / 60, ' minutes')
     return model
-
 
 def train_DDPG(env_train, model_name, timesteps=10000):
     """DDPG model"""
@@ -78,26 +93,6 @@ def train_PPO(env_train, model_name, timesteps=50000):
     model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
     print('Training time (PPO): ', (end - start) / 60, ' minutes')
     return model
-
-def train_GAIL(env_train, model_name, timesteps=1000):
-    """GAIL Model"""
-    #from stable_baselines.gail import ExportDataset, generate_expert_traj
-    start = time.time()
-    # generate expert trajectories
-    model = SAC('MLpPolicy', env_train, verbose=1)
-    generate_expert_traj(model, 'expert_model_gail', n_timesteps=100, n_episodes=10)
-
-    # Load dataset
-    dataset = ExpertDataset(expert_path='expert_model_gail.npz', traj_limitation=10, verbose=1)
-    model = GAIL('MLpPolicy', env_train, dataset, verbose=1)
-
-    model.learn(total_timesteps=1000)
-    end = time.time()
-
-    model.save(f"{config.TRAINED_MODEL_DIR}/{model_name}")
-    print('Training time (PPO): ', (end - start) / 60, ' minutes')
-    return model
-
 
 def DRL_prediction(df,
                    model,
@@ -148,6 +143,131 @@ def get_validation_sharpe(iteration):
              df_total_value['daily_return'].std()
     return sharpe
 
+# AR new
+def run_a2c_strategy(df, unique_trade_date, rebalance_window, validation_window) -> None:
+    """
+    A2C strats
+    
+    Parameters:
+    -----------
+    df : pd.DataFrame
+        full data set (done_data.csv)
+    unique_trade_date : numpy.ndarray
+        (unique) validation dates 
+    rebalance_window : int
+        number of months to retrain the model
+    validation_window : int
+        number of months to validation the model and select for trading
+    """
+    print("============Start A2C============")
+    last_state_ensemble = []
+    a2c_sharpe_list = []
+    a2c_custom_policy_sharpe_list = []
+
+    # based on the analysis of the in-sample data
+    # turbulence_threshold = 140
+    insample_turbulence = df[(df.datadate>=20090000) & (df.datadate<20151000)]
+    insample_turbulence = insample_turbulence.drop_duplicates(subset=['datadate'])
+    insample_turbulence_threshold = np.quantile(insample_turbulence.turbulence.values, .90)
+
+    start = time.time()
+    for i in range(rebalance_window + validation_window, len(unique_trade_date), rebalance_window):
+        print("============================================")
+        ## initial state is empty
+        if i - rebalance_window - validation_window == 0:
+            # inital state
+            initial = True
+        else:
+            # previous state
+            initial = False
+
+        # Tuning trubulence index based on historical data
+        # Turbulence lookback window is one quarter
+        end_date_index = df.index[df["datadate"] == unique_trade_date[i - rebalance_window - validation_window]].to_list()[-1]
+        start_date_index = end_date_index - validation_window*30 + 1
+
+        historical_turbulence = df.iloc[start_date_index:(end_date_index + 1), :]
+        #historical_turbulence = df[(df.datadate<unique_trade_date[i - rebalance_window - validation_window]) & (df.datadate>=(unique_trade_date[i - rebalance_window - validation_window - 63]))]
+
+        historical_turbulence = historical_turbulence.drop_duplicates(subset=['datadate'])
+
+        historical_turbulence_mean = np.mean(historical_turbulence.turbulence.values)
+
+        if historical_turbulence_mean > insample_turbulence_threshold:
+            # if the mean of the historical data is greater than the 90% quantile of insample turbulence data
+            # then we assume that the current market is volatile,
+            # therefore we set the 90% quantile of insample turbulence data as the turbulence threshold
+            # meaning the current turbulence can't exceed the 90% quantile of insample turbulence data
+            turbulence_threshold = insample_turbulence_threshold
+        else:
+            # if the mean of the historical data is less than the 90% quantile of insample turbulence data
+            # then we tune up the turbulence_threshold, meaning we lower the risk
+            turbulence_threshold = np.quantile(insample_turbulence.turbulence.values, 1)
+        print("turbulence_threshold: ", turbulence_threshold)
+
+        ############## Environment Setup starts ##############
+        ## training env -> it's from the first date till the start of the validation period 
+        train = data_split(df, start=20090000, end=unique_trade_date[i - rebalance_window - validation_window])
+        env_train = DummyVecEnv([lambda: StockEnvTrain(train)])
+
+        ## validation env
+        validation = data_split(df, start=unique_trade_date[i - rebalance_window - validation_window],
+                                end=unique_trade_date[i - rebalance_window])
+        env_val = DummyVecEnv([lambda: StockEnvValidation(validation,
+                                                          turbulence_threshold=turbulence_threshold,
+                                                          iteration=i)])
+        obs_val = env_val.reset()
+        ############## Environment Setup ends ##############
+
+        ############## Training and Validation starts ##############
+        print("======Model training from: ", 20090000, "to ",
+              unique_trade_date[i - rebalance_window - validation_window])
+        
+        print("======A2C Training========")
+        model_a2c = train_A2C(env_train, model_name="new_A2C_30k_dow_{}".format(i), timesteps=30000)
+
+        print("======A2C Validation from: ", unique_trade_date[i - rebalance_window - validation_window], "to ",
+              unique_trade_date[i - rebalance_window])
+        
+        DRL_validation(model=model_a2c, test_data=validation, test_env=env_val, test_obs=obs_val)
+        sharpe_a2c = get_validation_sharpe(i)
+        print("A2C Sharpe Ratio: ", sharpe_a2c)
+
+
+        #print("======A2C Custom Policy Training========")
+        #model_a2c_custom_policy = train_A2C_custom_policy(env_train, model_name="new_custom_policy_A2C_30k_dow_{}".format(i), timesteps=30000)
+
+        #print("======A2C Validation from: ", unique_trade_date[i - rebalance_window - validation_window], "to ",
+        #      unique_trade_date[i - rebalance_window])
+        
+        #DRL_validation(model=model_a2c_custom_policy, test_data=validation, test_env=env_val, test_obs=obs_val)
+        #sharpe_a2c_custom_policy = get_validation_sharpe(i)
+        #print("A2C Custom Policy Sharpe Ratio: ", sharpe_a2c_custom_policy)
+        #a2c_sharpe_list.append(sharpe_a2c)
+        
+        ############## Training and Validation ends ##############
+
+        ############## Trading starts ##############
+        print("======Trading from: ", unique_trade_date[i - rebalance_window], "to ", unique_trade_date[i])
+        #print("Used Model: ", model_ensemble)
+        last_state_ensemble = DRL_prediction(df=df, model=model_a2c, name="AR_A2C",
+                                             last_state=last_state_ensemble, iter_num=i,
+                                             unique_trade_date=unique_trade_date,
+                                             rebalance_window=rebalance_window,
+                                             turbulence_threshold=turbulence_threshold,
+                                             initial=initial)
+        # print("============Trading Done============")
+        ############## Trading ends ##############
+
+    end = time.time()
+    print("Ensemble Strategy took: ", (end - start) / 60, " minutes")
+
+
+
+
+
+
+##### --------------------------------- DONT TOUCH IT
 
 def run_ensemble_strategy(df, unique_trade_date, rebalance_window, validation_window) -> None:
     """Ensemble Strategy that combines PPO, A2C and DDPG"""
